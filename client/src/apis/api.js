cc2342a932eed969b91dad418b6a4acffdd480b5
Cheolwon Hwang <82077853+Cheol-Cheol@users.noreply.github.com>
2023-07-19 15:42:33 +0900

    url 수정

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.0.85:8080';
+const BASE_URL = 'http://43.201.10.6:8080';
 // const BASE_URL = 'http://localhost:8080/';
 
 export const getclassNameAndSemiarType = async () => {
